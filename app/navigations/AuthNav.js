ec9e20d0698b49ddd7014f239faea5eb7f92ad10
siddhesh-06 <61205098+siddhesh-06@users.noreply.github.com>
2021-03-12 12:18:27 +0530

    node modules

--- app/navigations/AuthNav.js
+++ app/navigations/AuthNav.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "../screens/LoginScreen";
-import RegisterScreen from "../screens/RegisterScreen";
-import DoctorPatientScreen from '../screens/DoctorPatientScreen';
+import RegisterPatientScreen from "../screens/RegisterScreen";
+import RegisterDoctorScreen from '../screens/RegisterDoctorScreen';
 
 const Stack = createStackNavigator();
 
 const AuthNav = () => (
   <Stack.Navigator initialRouteName="Login">
     <Stack.Screen name="Login" component={LoginScreen} />
-    <Stack.Screen name="DoctorPatientScreen" component={DoctorPatientScreen} />
-    <Stack.Screen name="Register" component={RegisterScreen} />
+    <Stack.Screen name="RegisterDoctorScreen" component={RegisterDoctorScreen} />
+    <Stack.Screen name="RegisterPatientScreen" component={RegisterPatientScreen} />
   </Stack.Navigator>
 );
 
