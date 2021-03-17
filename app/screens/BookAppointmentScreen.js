f04bb3762ccf0f645b4ee0cee9e0ce1b6c433bcf
Mayur Jain <maxcharge788769@gmail.com>
2021-03-17 14:53:04 +0530

    BookAppointmentScreen Second commit

--- app/screens/BookAppointmentScreen.js
+++ app/screens/BookAppointmentScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {View,StyleSheet} from "react-native";
-import { Button, Input , Layout , Datepicker, Text, RadioGroup, Radio} from "@ui-kitten/components";
+import { Button, Input , Datepicker, Text, RadioGroup, Radio, Icon} from "@ui-kitten/components";
 import { Formik } from "formik";
 import * as Yup from "yup";
 
@@ -8,10 +8,11 @@ import ErrorMsg from "../components/ErrorMsg";
 import { ScrollView } from "react-native-gesture-handler";
 
 const validationSchema = Yup.object().shape({
-  FirstName: Yup.string().required().label("FirstName"),
-  LastName: Yup.string().required().label("LastName"),
-  ContactNo: Yup.number().typeError().positive().integer().min(8).required().label("ContactNo"),
-  Age: Yup.number().required().positive().integer().label("Age"),
+  FirstName: Yup.string().required("Please Enter your First Name.").label(),
+  LastName: Yup.string().required("Please Enter your Last Name.").label(),
+  ContactNo: Yup.number().typeError().positive().integer().min(8).required("Please Enter your Contact number.").label(),
+  Age: Yup.number().required().positive().integer("Please enter your Age").label(),
+  Gender: Yup.string().required("Please select Gender").label(),
 });
 
 function BookAppointmentScreen(props) {
@@ -23,94 +24,113 @@ function BookAppointmentScreen(props) {
     }
   };
 
+  const [selectedIndex, setSelectedIndex] = React.useState(null);
+
+  const Gender=["Male","Female","Others"];
+
   const [date, setDate] = React.useState(new Date());
 
-  const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const CalendarIcon = (props) => (
+    <Icon {...props} name='calendar'/>
+  )
 
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Text style={styles.heading} category="h2" {...props} >Book {"\n"} Appointment</Text>
-        <Formik
-          initialValues={{
-            FirstName: "",
-            LastName: "",
-            ContactNo: "",
-            Age: "",
-            Gender: "",
-            Symptoms: "", 
-          }}
-          onSubmit={onRegister}
-          validationSchema={validationSchema}>
+        <Text style={styles.heading} category="h3" {...props} >Book Appointment</Text>
+          <Formik
+            initialValues={{
+              FirstName: "",
+              LastName: "",
+              ContactNo: "",
+              Age: "",
+              Gender: Gender[selectedIndex],
+              Symptoms: "",
+              AppointmentDate: date.toLocaleDateString(), 
+            }}
+            onSubmit={onRegister}
+            validationSchema={validationSchema}>
 
-          {({ handleChange, handleSubmit, errors }) => (
-            <>
-                <View style={styles.formContainer}>
-                    
-                    <Input style={styles.inputFields}
-                        label="First Name"
-                        placeholder="Enter your First Name"
-                        onChangeText={handleChange("FirstName")}
-                    />
-                    <ErrorMsg>{errors.FirstName}</ErrorMsg>
+            {({ handleChange, handleSubmit, errors }) => (
+              <>
+                  <View style={styles.formContainer}>
+                      
+                      <Input style={styles.inputFields}
+                          label="First Name"
+                          placeholder="Enter your First Name"
+                          onChangeText={handleChange("FirstName")}
+                      />
+                      <ErrorMsg>{errors.FirstName}</ErrorMsg>
 
-                    <Input style={styles.inputFields}
-                        label="Last Name"
-                        placeholder="Enter your Last name"
-                        onChangeText={handleChange("LastName")}
-                    />
-                    <ErrorMsg>{errors.LastName}</ErrorMsg>
+                      <Input style={styles.inputFields}
+                          label="Last Name"
+                          placeholder="Enter your Last name"
+                          onChangeText={handleChange("LastName")}
+                      />
+                      <ErrorMsg>{errors.LastName}</ErrorMsg>
 
-                    <Input style={[styles.inputFields]}
-                        label="Contact Number"
-                        placeholder="Enter your Mobile Number"
-                        onChangeText={handleChange("ContactNo")}
-                    />
-                    <ErrorMsg>{errors.ContactNo}</ErrorMsg>
+                      <Input style={[styles.inputFields]}
+                          label="Contact Number"
+                          placeholder="Enter your Mobile Number"
+                          onChangeText={handleChange("ContactNo")}
+                      />
+                      <ErrorMsg>{errors.ContactNo}</ErrorMsg>
 
-                    <Layout style={[styles.inputFields]} level='1'>
+                      <Input style={styles.inputFields}
+                          label="Age"
+                          placeholder="Enter your Age"
+                          onChangeText={handleChange("Age")}
+                      />
+                      <ErrorMsg>{errors.Age}</ErrorMsg>
+
+                      <View style={[styles.inputFields]}>
+                          <Text appearance="hint" style={{fontSize:12}}>
+                              Gender
+                          </Text>
+                          <RadioGroup
+                                style={{flex:9, flexDirection:"row"}}
+                                selectedIndex={selectedIndex}
+                                onChange={index => setSelectedIndex(index)}>
+                              <Radio style={{flex:3}}>Female</Radio>
+                              <Radio style={{flex:3}}>Male</Radio>
+                              <Radio style={{flex:3}}>Other</Radio>
+                          </RadioGroup>
+                      </View>
+                      <ErrorMsg>{errors.Gender}</ErrorMsg>
+
+                      <View style={[styles.inputFields, {marginBottom:15}]}>
+
                         <Text appearance="hint" style={{fontSize:12}}>
-                            Date Of Birth
+                          Select Appointment date
                         </Text>
+
                         <Datepicker
-                            style={{alignSelf:"stretch"}}
-                            date={date}
-                            onSelect={nextDate => setDate(nextDate)}
+                          accessoryRight={CalendarIcon}
+                          date={date}
+                          onSelect={nextDate => setDate(nextDate)}
                         />
-                    </Layout>
 
-                    <Layout style={[styles.inputFields]} level='1'>
-                        <Text appearance="hint" style={{fontSize:12}}>
-                            Gender
-                        </Text>
-                        <RadioGroup
-                            style={{flex:9, flexDirection:"row"}}
-                            selectedIndex={selectedIndex}
-                            onChange={index => setSelectedIndex(index)}>
-                            <Radio style={{flex:3}}>Female</Radio>
-                            <Radio style={{flex:3}}>Male</Radio>
-                            <Radio style={{flex:3}}>Other</Radio>
-                        </RadioGroup>
-                    </Layout>
+                      </View>
 
-                <Input
-                    multiline={true}
-                    style={[styles.inputFields,{maxHeight:100, marginBottom:30}]}
-                    label="Symptoms"
-                    placeholder="Enter Symptoms you are having"
-                    onChangeText={handleChange("Symptoms")}
-                />
+                      <Input
+                          multiline={true}
+                          textStyle={[styles.inputFields,{ minHeight:70, marginBottom:30, width:320 }]}
+                          label="Symptoms"
+                          placeholder="Enter Symptoms you are having"
+                          onChangeText={handleChange("Symptoms")}
+                      />
 
-                </View>
-                
-                <Button
-                    style={styles.btns}
-                    onPress={handleSubmit}>
-                    Submit
-                </Button>
-            </>
-          )}
-        </Formik>
+
+                  </View>
+                  
+                  <Button
+                      style={styles.btns}
+                      onPress={handleSubmit}>
+                      Submit
+                  </Button>
+              </>
+            )}
+          </Formik>
       </View>
     </ScrollView>
   );
@@ -118,18 +138,18 @@ function BookAppointmentScreen(props) {
 
 const styles = StyleSheet.create({
     container: {
-      marginVertical:100,
+      marginVertical:50,
       marginHorizontal:20,
     },
   formContainer:{
     
   },
   heading:{
-    top:-50,
+    top:-30,
     textAlign:"center",
   },
   inputFields: {
-   width:300,
+   width:320,
   },
   btns: {
     marginTop:20,
