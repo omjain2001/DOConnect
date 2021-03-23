8e62faa0eb56083c7acc1727e0a1fb0b8d93747e
Om Jain <omjain2606@gmail.com>
2021-03-23 16:49:38 +0530

    Created Form,FormField,SubmitForm button

---
+++ app/components/forms/FormField.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Layout, Input } from "@ui-kitten/components";
+import { useFormikContext } from "formik";
+import ErrorMsg from "../ErrorMsg";
+
+const FormField = ({ label, placeholder, name, style, ...otherProps }) => {
+  const {
+    setFieldValue,
+    errors,
+    values,
+    touched,
+    setFieldTouched,
+  } = useFormikContext();
+
+  return (
+    <Layout style={[styles.container, style]}>
+      <Input
+        label={label}
+        name={name}
+        value={values[name]}
+        placeholder={placeholder}
+        onChangeText={(value) => setFieldValue(name, value)}
+        onBlur={() => setFieldTouched(name)}
+        {...otherProps}
+      />
+      {errors[name] && (
+        <ErrorMsg visible={touched[name]}>{errors[name]}</ErrorMsg>
+      )}
+    </Layout>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    // paddingVertical: 60,
+    // width: 100,
+    paddingHorizontal: 5,
+    marginVertical: 10,
+  },
+});
+
+export default FormField;
