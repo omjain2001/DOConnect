14efa741118c6cf9b5e75c0f8ef7f5681998ee3d
Mayur Jain <maxcharge788769@gmail.com>
2021-05-22 11:17:48 +0530

    merged

--- app/redux/reducers/index.js
+++ app/redux/reducers/index.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-import moduleName from 'redux';
-=======
 import { combineReducers } from "redux";
 
 const reducers = combineReducers({
@@ -8,4 +5,3 @@ const reducers = combineReducers({
 });
 
 export default reducers;
->>>>>>> 994a83632b7a27de44578a0be7880c526b8a2df0
